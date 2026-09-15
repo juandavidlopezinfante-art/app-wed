df43ff84f1c63a12ef0fe73bd190ad1608d590f6
juandavidlopezinfante-art <juandavidlopezinfante@gmail.com>
2026-09-14 20:46:56 -0600

    Update inicio.py

--- inicio.py
+++ inicio.py
@@ -19,7 +19,7 @@ app = Flask(__name__)
 # Configuración de Variables de Entorno y Claves API
 HF_API_TOKEN = os.environ.get("HF_API_TOKEN")
 GEMINI_API_KEY = os.environ.get("GEMINI_API_KEY")
-HF_API_URL = "https://api-inference.huggingface.co/models/black-forest-labs/FLUX.1-schnell"
+HF_API_URL = "https://router.huggingface.co/hf-inference/models/black-forest-labs/FLUX.1-schnell"
 PRO_SECRET_KEY = os.environ.get("PRO_SECRET_KEY", "PRO-150-ACTIVO")
 PROMO_3_MESES_KEY = os.environ.get("PROMO_3_MESES_KEY", "PROMO-99-3MESES")
 STATIC_DIR = os.path.join(app.root_path, 'static')
