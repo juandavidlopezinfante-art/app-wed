8c0b226554c225354fa21176e1aa0d86dcea9e5a
juandavidlopezinfante-art <juandavidlopezinfante@gmail.com>
2026-09-14 18:56:24 -0600

    Update inicio.py

--- inicio.py
+++ inicio.py
@@ -21,7 +21,7 @@ HF_API_TOKEN = os.environ.get("HF_API_TOKEN")
 GEMINI_API_KEY = os.environ.get("GEMINI_API_KEY")
 HF_API_URL = os.environ.get(
     "HF_API_URL",
-    "https://router.huggingface.co/hf-inference/models/black-forest-labs/FLUX.1-schnell"
+    "https://api-inference.huggingface.co/models/black-forest-labs/FLUX.1-schnell"
 )
 PRO_SECRET_KEY = os.environ.get("PRO_SECRET_KEY", "PRO-150-ACTIVO")
 PROMO_3_MESES_KEY = os.environ.get("PROMO_3_MESES_KEY", "PROMO-99-3MESES")
